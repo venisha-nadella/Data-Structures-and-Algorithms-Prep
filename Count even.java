c11489585024d70dbdf7f6d8f8ca5f006f8fd618
Venisha Nadella <2200031688cseh@gmail.com>
2026-01-03 20:23:16 +0530

    Problem-5 done
    Count the number of even numbers in an array.

---
+++ Count even.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a[]=new int[]{1,2,3,4,5};
+        int count=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0){
+                count++;
+            }
+        }
+        System.out.print("No of even no.s="+count);
+    }
+}
