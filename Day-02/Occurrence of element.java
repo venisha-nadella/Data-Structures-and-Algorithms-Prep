5339b980973be6e8c4a56e541c3185c2029d2f60
Venisha Nadella <2200031688cseh@gmail.com>
2026-01-03 20:54:54 +0530

    Problem-12 done

---
+++ Day-02/Occurrence of element.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a[]=new int[5];
+        for(int i=0;i<5;i++){
+            a[i]=sc.nextInt();
+        }
+        int key=sc.nextInt();
+        int count=0;
+        for(int i=0;i<5;i++){
+            if(a[i]==key){
+                count++;
+            }
+        }
+        System.out.println(key+" occurres "+count+" times");
+    }
+}
