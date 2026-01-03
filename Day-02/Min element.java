d8597dea919fee91a323ac2b702c2975b242e313
Venisha Nadella <2200031688cseh@gmail.com>
2026-01-03 20:20:56 +0530

    Problem-4 done

---
+++ Day-02/Min element.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a[]=new int[]{1,2,3,4};
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<a.length;i++){
+            if(a[i]<min){
+                min=a[i];
+            }
+        }
+        System.out.print("Min="+min);
+    }
+}
