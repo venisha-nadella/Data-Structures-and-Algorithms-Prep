0dcaa918c2a88b262813ad593147e75b0c2cf81a
Venisha Nadella <2200031688cseh@gmail.com>
2026-01-03 20:08:04 +0530

    Problem-1 done

---
+++ Day-02/Print elements.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int a[]=new int[]{1,2,3,4};
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
