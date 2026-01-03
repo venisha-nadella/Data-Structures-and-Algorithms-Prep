5c695739e6e09e45b1701ad09c9e4c3df1c54672
Venisha Nadella <2200031688cseh@gmail.com>
2026-01-03 20:09:50 +0530

    Problem-2 done

---
+++ Day-02/Sum elements.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int a[]=new int[]{1,2,3,4};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+        System.out.print("Sum="+sum);
+    }
+}
